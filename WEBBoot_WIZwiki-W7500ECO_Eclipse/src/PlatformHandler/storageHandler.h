4a2f1f213108dc2dfef0ba7a1945eee607f07c80
Jim Jun <jim@wiznet.co.kr>
2015-11-05 13:09:26 +0900

    Add a feature of EEPROM_ENABLE.

--- WEBBoot_WIZwiki-W7500ECO_Eclipse/src/PlatformHandler/storageHandler.h
+++ WEBBoot_WIZwiki-W7500ECO_Eclipse/src/PlatformHandler/storageHandler.h
@@ -9,8 +9,12 @@
 #define STORAGEHANDLER_H_
 
 #include <stdint.h>
+#include "W7500x_i2c.h"
 
 int read_storage(uint8_t isConfig, void *data, uint16_t size);
 int write_storage(uint8_t isConfig, void *data, uint16_t size);
+
+extern I2C_ConfigStruct conf;
+extern void delay(__IO uint32_t milliseconds);
 
 #endif /* STORAGEHANDLER_H_ */
